775972e30fda2b14a5e2d6a9e5501c1bf2567ca3
Shawn Yoon <hysy9255@gmail.com>
2023-02-15 19:09:41 +0900

    edit

--- week1-day2/main.js
+++ week1-day2/main.js
@@ -1,3 +1,19 @@
+// reverse 함수에 정수인 숫자를 인자로 받습니다.
+// 그 숫자를 뒤집어서 return해주세요.
+
+// x: 숫자
+// return: 뒤집어진 숫자를 반환!
+
+// 예들 들어,
+// x: 1234
+// return: 4321
+
+// x: -1234
+// return: -4321
+
+// x: 1230
+// return: 321
+
 const reverse = (x) => {
   let stringX = String(x);
   let arr = [];
